d6f3aa1a2c30022440489ee044b20438bb71337f
ANDUS International <info@andusinternationalng.com>
2026-05-23 11:30:17 +0100

    fix css and config for next 16

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,12 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "plus.unsplash.com" },
     ],
-    formats: ["image/avif", "image/webp"],
   },
 };
 
